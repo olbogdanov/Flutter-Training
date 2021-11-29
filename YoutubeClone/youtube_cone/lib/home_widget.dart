89d26f50e5f706ca6eb509923df933c7ac118f2f
bogdanov <iceposhta@gmail.com>
2021-11-29 10:14:42 +0100

    extract home widget to a separate file

---
+++ YoutubeClone/youtube_cone/lib/home_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    var spacecrafts = [
+      "James Web",
+      "Enterprise",
+      "Hubble",
+      "Kepler",
+      "Juno",
+      "Casini",
+      "Columbia",
+      "Challenger",
+      "Huygens",
+      "Galileo",
+      "Apollo",
+      "Spitzer",
+      "WMAP",
+      "Swift",
+      "Atlantis"
+    ];
+    var listItem = ListView.builder(
+      itemCount: spacecrafts.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Card(
+            elevation: 5.0,
+            child: Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Text(spacecrafts[index]),
+            ),
+          ),
+          onTap: () {},
+        );
+      },
+    );
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Flutter ListView")),
+      body: listItem,
+    );
+  }
+}
--- YoutubeClone/youtube_cone/lib/main.dart
+++ YoutubeClone/youtube_cone/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_cone/home_widget.dart';
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -6,60 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter ListView',
+      title: 'Youtube Home',
       home: const Home(),
       theme: ThemeData(primaryColor: Colors.orange),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    var spacecrafts = [
-      "James Web",
-      "Enterprise",
-      "Hubble",
-      "Kepler",
-      "Juno",
-      "Casini",
-      "Columbia",
-      "Challenger",
-      "Huygens",
-      "Galileo",
-      "Apollo",
-      "Spitzer",
-      "WMAP",
-      "Swift",
-      "Atlantis"
-    ];
-    var listItem = ListView.builder(
-      itemCount: spacecrafts.length,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Card(
-            elevation: 5.0,
-            child: Container(
-              alignment: Alignment.centerLeft,
-              margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-              child: Text(spacecrafts[index]),
-            ),
-          ),
-          onTap: () {},
-        );
-      },
-    );
-
-    return Scaffold(
-      appBar: AppBar(title: const Text("Flutter ListView")),
-      body: listItem,
     );
   }
 }
