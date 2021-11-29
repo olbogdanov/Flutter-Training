a05280ed16c3e83a6f3ff5cec96aec112bb087d9
Max Klein <max.klein@grandcentrix.net>
2021-11-29 10:10:43 +0100

    refactor

--- YoutubeClone/youtube_cone/lib/main.dart
+++ YoutubeClone/youtube_cone/lib/main.dart
@@ -1,54 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Flutter ListView',
-      home: new Home(),
-      theme: new ThemeData(primaryColor: Colors.orange),
+      home: const Home(),
+      theme: ThemeData(primaryColor: Colors.orange),
     );
   }
 }
 
 class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
-  _HomeState createState() => new _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   @override
   Widget build(BuildContext context) {
-    var spacecrafts = ["James Web","Enterprise","Hubble","Kepler","Juno","Casini","Columbia","Challenger","Huygens","Galileo","Apollo","Spitzer","WMAP","Swift","Atlantis"];
-    var listItem = new ListView.builder(
+    var spacecrafts = [
+      "James Web",
+      "Enterprise",
+      "Hubble",
+      "Kepler",
+      "Juno",
+      "Casini",
+      "Columbia",
+      "Challenger",
+      "Huygens",
+      "Galileo",
+      "Apollo",
+      "Spitzer",
+      "WMAP",
+      "Swift",
+      "Atlantis"
+    ];
+    var listItem = ListView.builder(
       itemCount: spacecrafts.length,
       itemBuilder: (BuildContext context, int index) {
-        return new ListTile(
-          title: new Card(
+        return ListTile(
+          title: Card(
             elevation: 5.0,
-            child: new Container(
+            child: Container(
               alignment: Alignment.centerLeft,
-              margin: new EdgeInsets.only(top: 10.0, bottom: 10.0),
-              child: new Text(spacecrafts[index]),
+              margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Text(spacecrafts[index]),
             ),
           ),
-          onTap: () {
-
-          },
+          onTap: () {},
         );
       },
     );
 
-    return new Scaffold(
-      appBar: new AppBar(
-          title: new Text("Flutter ListView")
-      ),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Flutter ListView")),
       body: listItem,
     );
   }
 }
+
 void main() {
-  runApp(new MyApp());
+  runApp(MyApp());
 }
