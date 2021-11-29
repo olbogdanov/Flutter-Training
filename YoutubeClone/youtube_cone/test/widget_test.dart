99205cfb4c0e09ab126c48f44af76608c0684133
bogdanov <iceposhta@gmail.com>
2021-11-29 17:06:17 +0100

    test of the subscribe button

--- YoutubeClone/youtube_cone/test/widget_test.dart
+++ YoutubeClone/youtube_cone/test/widget_test.dart
@@ -9,22 +9,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:youtube_cone/main.dart';
+import 'package:youtube_cone/video_detail.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+
+    tester.binding.window.physicalSizeTestValue = Size(400, 1000);
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MaterialApp(home: VideoDetail(),));
+    await tester.dragUntilVisible(find.byType(TextButton), find.byType(SingleChildScrollView), Offset(0, 50));
+
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Unsubscribe'), findsOneWidget);
+    expect(find.text('Subscribe'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byType(TextButton));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Unsubscribe'), findsNothing);
+    expect(find.text('Subscribe'), findsOneWidget);
   });
 }
