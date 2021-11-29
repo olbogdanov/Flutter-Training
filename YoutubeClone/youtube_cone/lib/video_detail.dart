c71a74e2755db86bdb7aea0b5ac03e93317b0f4a
Max Klein <max.klein@grandcentrix.net>
2021-11-29 11:19:16 +0100

    added appbar

--- YoutubeClone/youtube_cone/lib/video_detail.dart
+++ YoutubeClone/youtube_cone/lib/video_detail.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class VideoDetail extends StatelessWidget {
   const VideoDetail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(title: const Text('Video Detail')), body: Container());
   }
 }
