0c821395bc2547be34098cb46caa635ccf146588
Samira Vaezi <samira.vaezi@grandcentrix.net>
2021-11-29 12:40:22 +0100

    decorated video title and action menus

--- YoutubeClone/youtube_cone/lib/video_detail.dart
+++ YoutubeClone/youtube_cone/lib/video_detail.dart
@@ -13,37 +13,45 @@ class VideoDetail extends StatelessWidget {
 }
 
 class Detail extends StatelessWidget {
-  const Detail({Key? key,}) : super(key: key);
+  const Detail({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return
-      SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            AspectRatio(
-              aspectRatio: 16/9,
-              child: Container(
-                color: Colors.blueAccent,
-                child:
-                IconButton(onPressed: () {}, icon: const Icon(Icons.play_arrow)),
-              ),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Container(
+              color: Colors.blueAccent,
+              child: IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.play_arrow)),
             ),
-            Text("Title of the video"),
-            Container(
-              width: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Icon(Icons.thumb_up),
-                  Icon(Icons.thumb_down),
-                ],
-              ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text("Title of the video"),
+          ),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(children: const [Icon(Icons.thumb_up), Text("4.2k")]),
+                Column(
+                    children: const [Icon(Icons.thumb_down), Text("Dislike")]),
+                Column(children: const [Icon(Icons.share), Text("Share")]),
+                Column(children: const [Icon(Icons.save), Text("Save")])
+              ],
             ),
-            RecommendedVideos()
-          ],
-        ),
-      );
+          ),
+          const RecommendedVideos()
+        ],
+      ),
+    );
   }
 }
