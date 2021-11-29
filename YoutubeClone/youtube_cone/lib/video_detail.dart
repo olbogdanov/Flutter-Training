16af08cc38d94aceac571916dcff57856368e380
bogdanov <iceposhta@gmail.com>
2021-11-29 11:36:13 +0100

    add preview and footer to video_detail

--- YoutubeClone/youtube_cone/lib/video_detail.dart
+++ YoutubeClone/youtube_cone/lib/video_detail.dart
@@ -7,6 +7,38 @@ class VideoDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Video Detail')), body: Container());
+        appBar: AppBar(title: const Text('Video Detail')), body: Detail());
+  }
+}
+
+class Detail extends StatelessWidget {
+  const Detail({Key? key,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: double.infinity,
+            height: 300,
+            color: Colors.blueAccent,
+            child:
+            IconButton(onPressed: () {}, icon: const Icon(Icons.play_arrow)),
+          ),
+          Text("Title of the video"),
+          Container(
+            width: double.infinity,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Icon(Icons.thumb_up),
+                Icon(Icons.thumb_down),
+              ],
+            ),
+          )
+        ],
+      );
   }
 }
