c310831b6d452c2a1f23dd277e874f66a0e3076a
bogdanov <iceposhta@gmail.com>
2021-11-29 10:56:05 +0100

    add a footer with two texts and avatar

--- YoutubeClone/youtube_cone/lib/video_widget.dart
+++ YoutubeClone/youtube_cone/lib/video_widget.dart
@@ -6,12 +6,48 @@ class VideoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 400,
-      color: Colors.blueAccent,
-      child: IconButton(
-          onPressed: () {}, icon: const Icon(Icons.access_alarm_rounded)),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: double.infinity,
+          height: 400,
+          color: Colors.blueAccent,
+          child:
+              IconButton(onPressed: () {}, icon: const Icon(Icons.play_arrow)),
+        ),
+        VideoFooter(url: url),
+      ],
+    );
+  }
+}
+
+class VideoFooter extends StatelessWidget {
+  const VideoFooter({
+    Key? key,
+    required this.url,
+  }) : super(key: key);
+
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CircleAvatar(
+          radius: 28,
+          backgroundImage: NetworkImage(
+              "https://cdn1.vectorstock.com/i/1000x1000/23/70/man-avatar-icon-flat-vector-19152370.jpg"),
+          child: Stack(),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(url),
+            Text("some description"),
+          ],
+        ),
+      ],
     );
   }
 }
