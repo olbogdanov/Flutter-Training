e23596924ab923bd350d418825fbba424044eec0
bogdanov <iceposhta@gmail.com>
2021-11-30 15:34:27 +0100

    unit test of a products provider

--- YoutubeClone/shopping_cart_starter/test/model_test.dart
+++ YoutubeClone/shopping_cart_starter/test/model_test.dart
@@ -1,19 +1,33 @@
+import 'package:state_management_cart/ProductsProvider.dart';
+import 'package:state_management_cart/api/product.dart';
+import 'package:state_management_cart/shopingcart.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('cart', () {
     test("Add a product", () {
-      // TODO
+      final provider = ShoppingCardProvider();
+      var product = Product(name: "name", price: 22, imageUrl: "url");
+      provider.addProductListItem(product);
+      assert(provider.product.contains(product));
+      expect(provider.product.length, 1);
+      // assert()
     });
 
     test("Remove a product", () {
-      // TODO
+      final provider = ShoppingCardProvider();
+      var product = Product(name: "name", price: 22, imageUrl: "url");
+      provider.addProductListItem(product);
+      provider.removeProductListItem(product.name);
+      expect(provider.product.length, 0);
     });
   });
 
   group('products', () {
     test('load products', () async {
-      // TODO
+      final provider = ProductsProvider();
+      await provider.loadData();
+      assert(provider.products.isNotEmpty);
     });
   });
 }
