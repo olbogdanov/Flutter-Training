da148fa7ac0747c0b510365c3df50d4e8f30002f
Max Klein <max.klein@grandcentrix.net>
2021-11-30 15:15:34 +0100

    shoping cart items

--- YoutubeClone/shopping_cart_starter/lib/cart_overview.dart
+++ YoutubeClone/shopping_cart_starter/lib/cart_overview.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 import 'package:state_management_cart/api/product.dart';
+import 'package:state_management_cart/shopingcart.dart';
 
 class CartOverview extends StatelessWidget {
   const CartOverview({Key? key}) : super(key: key);
@@ -8,7 +10,9 @@ class CartOverview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO get from ProductsModel somewhere
-    final List<Product> products = [];
+    ShoppingCardProvider shoppingCardProvider =
+        Provider.of<ShoppingCardProvider>(context);
+    final List<Product> products = shoppingCardProvider.product;
 
     if (products.isEmpty) {
       return const Center(
@@ -28,7 +32,7 @@ class CartOverview extends StatelessWidget {
             children: [
               TextButton(
                 onPressed: () {
-                  // TODO remove item from cart
+                  shoppingCardProvider.removeProductListItem(product.name);
                 },
                 child: const Text("Remove"),
               ),
