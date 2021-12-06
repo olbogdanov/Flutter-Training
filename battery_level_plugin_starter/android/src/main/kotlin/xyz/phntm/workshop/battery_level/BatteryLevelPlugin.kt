f354556066120aeca243318c2ddcf678cb6cbf45
bogdanov <iceposhta@gmail.com>
2021-12-06 11:25:09 +0100

    return the battery level of android

--- battery_level_plugin_starter/android/src/main/kotlin/xyz/phntm/workshop/battery_level/BatteryLevelPlugin.kt
+++ battery_level_plugin_starter/android/src/main/kotlin/xyz/phntm/workshop/battery_level/BatteryLevelPlugin.kt
@@ -1,7 +1,9 @@
 package xyz.phntm.workshop.battery_level
 
 import androidx.annotation.NonNull
-
+import android.content.Context
+import android.content.Context.BATTERY_SERVICE
+import android.os.BatteryManager
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -15,18 +17,22 @@ class BatteryLevelPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var context: Context
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "battery_level")
     channel.setMethodCallHandler(this)
+    context = flutterPluginBinding.applicationContext
+
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } if (call.method == "getBatteryLevel") {
+    } else if (call.method == "getBatteryLevel") {
       val bm = context.getSystemService(BATTERY_SERVICE) as BatteryManager
-      result.success("${bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)}")
+      val battery = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+      result.success("$battery")
     } else {
       result.notImplemented()
     }
