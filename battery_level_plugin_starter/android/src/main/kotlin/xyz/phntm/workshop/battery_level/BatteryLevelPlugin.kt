f19383897b91941b14b085fcab3c43be2730d281
bogdanov <iceposhta@gmail.com>
2021-12-06 11:47:57 +0100

    send battery info as a json from android

--- battery_level_plugin_starter/android/src/main/kotlin/xyz/phntm/workshop/battery_level/BatteryLevelPlugin.kt
+++ battery_level_plugin_starter/android/src/main/kotlin/xyz/phntm/workshop/battery_level/BatteryLevelPlugin.kt
@@ -9,6 +9,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import com.google.gson.Gson
 
 /** BatteryLevelPlugin */
 class BatteryLevelPlugin: FlutterPlugin, MethodCallHandler {
@@ -30,15 +31,28 @@ class BatteryLevelPlugin: FlutterPlugin, MethodCallHandler {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else if (call.method == "getBatteryLevel") {
-      val bm = context.getSystemService(BATTERY_SERVICE) as BatteryManager
-      val battery = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+      val battery = getCurrentBattery()
       result.success("$battery")
+    } else if (call.method == "getBatteryInformation") {
+      val battery = getCurrentBattery()
+      val gson = Gson()
+      val batteryInfo = BatteryInformation(battery, true)
+      val json: String = gson.toJson(batteryInfo)
+      result.success(json)
     } else {
       result.notImplemented()
     }
+  }
+
+  private fun getCurrentBattery(): Int {
+    val bm = context.getSystemService(BATTERY_SERVICE) as BatteryManager
+    val battery = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+    return battery
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
 }
+
+data class BatteryInformation(val batteryLevel: Int, val isCharing: Boolean)
